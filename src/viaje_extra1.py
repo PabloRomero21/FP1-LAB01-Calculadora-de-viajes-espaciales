9185e0098f6cf6b0b4f9922d669cef0d2a8495c1
PabloRomero21 <pabrommar@alum.us.es>
2025-09-26 17:01:57 +0200

    hgf

--- src/viaje_extra1.py
+++ src/viaje_extra1.py
@@ -1,8 +1,8 @@
 distancia = int(input("Introduce la distancia total en km:"))
 distancia_recorrida= 0
 parada = 0
-while distancia_recorrida <= distancia:
-    distancia_recorrida = distancia_recorrida + 150000
+while distancia_recorrida < distancia-15000:
+    distancia_recorrida = distancia_recorrida + 15000
     parada= parada+1
-    print(f"Parada en el km {disatncia_recorrida}")
+    print(f"Parada en el km {distancia_recorrida}")
 print(f"Total de paradas para respostar: {parada}")
